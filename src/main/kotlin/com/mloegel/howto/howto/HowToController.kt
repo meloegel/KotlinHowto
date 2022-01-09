b9ea8b33fdcd2ca03aabec9cf96881fc6fc29d92
meloegel <meloegel@gmail.com>
2022-01-09 16:36:34 -0500

    Added getHowtosByUser in HowToController

--- src/main/kotlin/com/mloegel/howto/howto/HowToController.kt
+++ src/main/kotlin/com/mloegel/howto/howto/HowToController.kt
@@ -1,9 +1,10 @@
 package com.mloegel.howto.howto
 
+import com.mloegel.howto.user.UserService
 import org.springframework.web.bind.annotation.*
 
 @RestController
-class HowToController(val service: HowToService) {
+class HowToController(val service: HowToService, val userService: UserService) {
     @GetMapping("/howtos")
     fun getAllHowtos(): MutableIterable<HowTo> = service.findHowtos()
 
@@ -12,6 +13,13 @@ class HowToController(val service: HowToService) {
 
     @GetMapping("/howtos/{category}")
     fun getHowtosByCategory(@PathVariable category: String): List<HowTo> = service.findHowtosByCategory(category)
+
+    @GetMapping("/howtos/{userid}")
+    fun getHowtosByUser(@PathVariable userid: Int): List<HowTo> {
+        val user = userService.findByUserid(userid)
+        return service.findHowtosByUser(user)
+    }
+
 
     @PostMapping("/howtos")
     fun postHowto(@RequestBody howTo: HowTo) {
